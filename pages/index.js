a9a31b77974dc7afaeec7a3f89b10289c4dbe6a9
Shubham Anand Gupta <shubhamanandgupta@outlook.com>
2022-06-15 07:54:32 +0530

    h

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div id={styles.lipi} className={styles.lipi + " my-2 mx-2 text-4xl font-semibold"}>
-        Lipi Lekhika
+      <div id={styles.lipi} className={`${styles.lipi} ${styles.lp} inline-block rounded-md px-2 py-2 lipi my-2 mx-2 text-4xl font-semibold`}>
+        Lipi Lekhika using Tailwind CSS
       </div>
     </div>
   )
