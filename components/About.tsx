8d4e09d67eb9873b2c541374123717e1178bb921
Shubham Anand Gupta <shubhamanandgupta@outlook.com>
2022-06-24 07:31:46 +0530

    k

--- components/About.tsx
+++ components/About.tsx
@@ -1,9 +1,9 @@
 import { NextPage } from 'next'
 import React from 'react'
-type about_valid= {
-    text: string
-}
-const About: NextPage<about_valid> = (pros) => {
+// type about_valid= {
+//     text: string
+// }
+const About: NextPage<{ text: string }> = (pros) => {
     return (
         <div className='text-red-900 hover:text-4xl'>About {pros.text}</div>
     )
