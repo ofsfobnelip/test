a992207bf665362462b5203243dd65f02a2f5398
Shubham Anand Gupta <shubhamanandgupta@outlook.com>
2022-06-24 12:17:45 +0530

    k

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { useState } from 'react'
 function MyApp({ Component, pageProps }: AppProps) {
+  const [tm, set_tm] = useState(0)
+  const HangleClick=()=>set_tm(tm+1)
   return <>
     <Head>
       <meta name="robots" content="noindex"></meta>
     </Head>
-    Test Parent Element 
+    Test Parent Element - {tm} seconds
+    <button className="border-2 border-black m-2 p-1" onClick={HangleClick}>Change</button>
     <Component {...pageProps} />
   </>
 }
