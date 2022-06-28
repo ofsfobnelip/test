7502907ff0d0c3e309d7b2a5c3f60fdcd9fdb584
Shubham Anand Gupta <shubhamanandgupta@outlook.com>
2022-06-28 11:29:09 +0530

    k

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import { Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 import { store } from '../state/store'
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
     <Head>
-      <meta name="robots" content="noindex"></meta>
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <meta name="robots" content="noindex" />
     </Head>
     <Provider store={store}>
       <Component {...pageProps} />
